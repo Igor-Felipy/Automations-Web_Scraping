832cf4a7cd973c6a9ade61f7bdc73a64ca130b95
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-14 16:20:35 -0400

    youtube bloquando

--- selnium/scripts/youtube_automator.py
+++ selnium/scripts/youtube_automator.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+
 a = 'flow fla flu'
-driver = webdriver.Chrome(executable_path=r'./chromedriver')
+
+opts = Options()
+opts.add_argument('user-agent=Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1')
+
+driver = webdriver.Chrome(executable_path='bot/selnium/scripts/chromedriver', chrome_options=opts)
 driver.get('https://www.youtube.com/')
 #//*[@id="search"] barra de pesquisa
 #//*[@id="search-icon-legacy"] botao de pesquisa
 #//*[@id="video-title"]/yt-formatted-string primeiro video
-time.sleep(5)
-barra = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div/div[1]')
+sleep(5)
+barra = driver.find_element_by_xpath('//*[@id="header-bar"]/header/div/button/c3-icon')
+sleep(1)
 barra.click()
-time.sleep(3)
-
+sleep(1)
 barra.send_keys(a)
-
+sleep(3)
 pesquisar = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
 pesquisar.click()
