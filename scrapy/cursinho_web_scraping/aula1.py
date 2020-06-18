7e733da4239a6fc047bcdccb8e2c3b456f1f9dbd
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-17 22:59:36 -0400

    buscando o nome o site

--- scrapy/cursinho_web_scraping/aula1.py
+++ scrapy/cursinho_web_scraping/aula1.py
@@ -6,7 +6,7 @@ url = ('https://multicanais.com/tvonline/')
 r = requests.get(url)
 html_content = r.text
 soup = BeautifulSoup(html_content, 'html.parser')
-lista_title = soup.find_all('title')
+lista_title = soup.find_all('title')#procura tudo que esta dentro de uma tag title
 for lista_dados in lista_title:
     print('Nome Do Site: ')
     print(lista_dados.next_element)
