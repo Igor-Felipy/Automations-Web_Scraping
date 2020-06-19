6a5ea891adfb56cde9d98fea8727a15bb2f90bb8
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-18 21:03:00 -0400

    buscador de dados completo

--- scrapy/cursinho_web_scraping/aula2.py
+++ scrapy/cursinho_web_scraping/aula2.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 from time import sleep
+import smtplib
+
 url = "https://www.coronatracker.com/pt-br/"
 
 driver = webdriver.Chrome('bot/scrapy/cursinho_web_scraping/chromedriver')
@@ -14,3 +16,29 @@ death = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div[1]/di
 
 
 print(f'Casos confirmados: {cases} \nRecuperados: {rec} \nMortos: {death}')
+
+
+
+def sendmail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('seu email', 'sua senha')
+
+    subject = 'Corre que ficou ruim'
+    body = 'CARA ACABOU DE ATINGIR: {} de casos confirmados, alem disso tambem {} morreram'.format(cases, death)
+    msg = 'Subject: {} {}'.format(subject,body)
+    server.sendmail(
+        'seu email',
+        'email de destino',
+        msg
+    )
+    print('EMAIL ENVIADO COM URGENCIA')
+    server.quit()
+
+
+
+if cases >= "3.000.000":
+    sendmail()
